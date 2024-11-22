9e940af08449da6934e9d6014ddddfe801a9c79e
akress <akress@unistra.fr>
2024-11-22 18:05:07 +0100

    Added docstring

--- galaxy_predictor.py
+++ galaxy_predictor.py
@@ -5,10 +5,20 @@ import os
 
 
 class CodonCounter:
+    """
+    Object to count codons in FASTA files
+    """
+
     def __init__(self):
         self.table = {key: 0 for key in self._generate_all_codons()}
 
     def _generate_all_codons(self):
+        """
+        Generate all possible codons
+
+        Returns:
+            list: a list of all possible codons
+        """
         codons = []
         for i in "ACGT":
             for j in "ACGT":
@@ -17,9 +27,21 @@ class CodonCounter:
         return codons
 
     def reset(self):
+        """
+        Reset the count table
+        """
         self.table = {key: 0 for key in self.table.keys()}
 
     def count_file(self, path):
+        """
+        Count codons in a FASTA file
+
+        Args:
+            path (str): the path to the FASTA file
+
+        Raises:
+            FileNotFoundError: if the file is not found
+        """
         try:
             for record in Bio.SeqIO.parse(path, "fasta"):
                 self.count_sequence(str(record.seq))
@@ -27,6 +49,12 @@ class CodonCounter:
             print(f"Error reading file {path}")
 
     def count_sequence(self, sequence):
+        """
+        Count codons in a sequence
+
+        Args:
+            sequence (str): the sequence to count
+        """
         if len(sequence) == 0:
             return
         for i in range(0, len(sequence) - 2, 1):
@@ -54,6 +82,15 @@ class GalaxyPredictor:
             raise FileNotFoundError("Model not found at path: " + self.model_path)
 
     def predict(self, toto):
+        """
+        Predicts the galaxy region for a given input.
+
+        Returns:
+            int: the predicted class
+
+        Raises:
+            FileNotFoundError: if the model is not found
+        """
         if not self.model:
             # load the model
             self._load_model()
@@ -61,6 +98,15 @@ class GalaxyPredictor:
         return self.model.predict([values])
 
     def get_label(self, value):
+        """
+        Get the label for a given class
+
+        Args:
+            value (int): the class value
+
+        Returns:
+            str: the class label
+        """
         return self.labels[value]
 
 
