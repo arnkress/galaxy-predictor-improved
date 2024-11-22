a521e2bef5a7250b2a927eaa966cfc27c3db790b
akress <akress@unistra.fr>
2024-11-22 14:44:33 +0100

    Add pre-commit lint and format

--- galaxy_predictor.py
+++ galaxy_predictor.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python3
-import random
 import Bio.SeqIO
+import joblib
+import os
+
+
 class codon_counter:
     def __init__(self):
-        self.table = {key:0 for key in self.generateAllcodons()}
+        self.table = {key: 0 for key in self.generateAllcodons()}
 
-    def  generateAllcodons(self):
+    def generateAllcodons(self):
         codons = []
-        for i in 'ACGT':
+        for i in "ACGT":
             for j in "ACGT":
                 for k in "ACGT":
-                    codons.append(i+j+k)
+                    codons.append(i + j + k)
         return codons
 
-    def reset(self):self.table = {key: 0 for key in self.table.keys()}
+    def reset(self):
+        self.table = {key: 0 for key in self.table.keys()}
 
-    def count_file(self,path):
+    def count_file(self, path):
         try:
             for record in Bio.SeqIO.parse(path, "fasta"):
                 self.count_sequence(str(record.seq))
-        except:
+        except FileNotFoundError:
             print(f"Error reading file {path}")
 
-    def count_sequence(self ,sequence):
-        if len(sequence) is 0:
+    def count_sequence(self, sequence):
+        if len(sequence) == 0:
             return
-        for i in range(0, len(sequence) -   3, 1):
+        for i in range(0, len(sequence) - 3, 1):
             self.table[sequence[i : i + 3]] += 1
 
     def get_table(self):
         return self.table
 
-import os, joblib
 
 class galaxyPredictor:
     labels = {
         0: "LOWER ARM",
-    1: "CENTER",
-    2: "UPPER ARM",
-    2: "UPPER ARM",
+        1: "CENTER",
+        2: "UPPER ARM",
     }
 
     def __init__(self, model_path=None):
@@ -49,18 +51,18 @@ class galaxyPredictor:
         if os.path.exists(self.model_path):
             self.model = joblib.load(self.model_path)
         else:
-            raise FileNotFoundError(f"Model not found at path: " + self.model_path)
+            raise FileNotFoundError("Model not found at path: " + self.model_path)
 
     def predict(self, toto):
-        tmp = 0
         if not self.model:
-                # load the model
+            # load the model
             self._load_model()
         values = [toto[key] for key in sorted(toto.keys())]
         return self.model.predict([values])
-    
+
     def get_label(self, value):
         return galaxyPredictor.labels[value]
+
 
 if __name__ == "__main__":
     cc = codon_counter()
